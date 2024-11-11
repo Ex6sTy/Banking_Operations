cb57140180456c2247e0374070be66742d2fe092
Artemi Bogatov <aboga99@mail.ru>
2024-11-11 15:03:23 +0300

    Исправление ошибок

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
-from typing import List, Dict
+from typing import Dict, List
 
-def filter_by_state(operations: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+
+def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Фильтрует список операций по заданному состоянию.
 
@@ -8,7 +9,8 @@ def filter_by_state(operations: List[Dict], state: str = 'EXECUTED') -> List[Dic
     :param state: Состояние, по которому требуется фильтровать (по умолчанию 'EXECUTED').
     :return: Список операций с заданным состоянием.
     """
-    return [operation for operation in operations if operation.get('state') == state]
+    return [operation for operation in operations if operation.get("state") == state]
+
 
 def sort_by_date(operations: List[Dict], descending: bool = True) -> List[Dict]:
     """
@@ -18,7 +20,4 @@ def sort_by_date(operations: List[Dict], descending: bool = True) -> List[Dict]:
     :param descending: Порядок сортировки (по умолчанию убывание).
     :return: Отсортированный список операций.
     """
-    return sorted(operations, key=lambda x: x['date'], reverse=descending)
-
-
-
+    return sorted(operations, key=lambda x: x["date"], reverse=descending)
